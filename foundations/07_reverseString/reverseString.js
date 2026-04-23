47244315dd2139ca767613c8292f1e7a47fcd5e0
LeonardoCo <57707316+LeonardoCo@users.noreply.github.com>
2026-04-23 15:19:42 +0200

    Implement string reversal in reverseString function
    Fix reverseString function to correctly reverse input string.

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
 const reverseString = function() {
-
+    let sum="";
+    for (let i = string.length-1; i >= 0; i-- ){
+        sum += string[i];
+    }
+    return sum;
 };
 
 // Do not edit below this line
