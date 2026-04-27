958ef5e6a44594bc4b03027a6bfc944cbc75e599
LeonardoCo <57707316+LeonardoCo@users.noreply.github.com>
2026-04-27 10:35:39 +0200

    Add convertToCelsius and convertToFahrenheit functions

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fWert) {
+    const ergebnis = (fWert-32)*5/9
+    const gerundet = Number(ergebnis.toFixed(1))
+    return gerundet
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cWert) {
+    const ergebnis = (cWert*9/5)+32
+    const gerundet = Number(ergebnis.toFixed(1))
+    return gerundet
 };
 
 // Do not edit below this line
